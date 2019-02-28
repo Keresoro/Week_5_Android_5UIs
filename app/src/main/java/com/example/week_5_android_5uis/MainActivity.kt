403859dc6fedf6c84551d122db450e29ff5d32fe
Fotis mitsingas <fotismitsingas@10-79-34-110-wdefault.solent.ac.uk>
2019-02-28 15:20:58 +0000

    Initial commit

---
+++ app/src/main/java/com/example/week_5_android_5uis/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.week_5_android_5uis
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
